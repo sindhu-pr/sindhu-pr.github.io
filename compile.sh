b82a7b7e24a57b143b43f85efd1cb550fb8d04e4
sindhupr <admin@localhost.localdomain>
2017-11-21 16:15:40 +0530

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,17 @@
+./jemdoc.py index.jemdoc 
+./jemdoc.py research.jemdoc 
+./jemdoc.py courses.jemdoc 
+./jemdoc.py ta.jemdoc 
+./jemdoc.py Thesis.jemdoc 
+./jemdoc.py publications.jemdoc 
+./jemdoc.py education.jemdoc 
+./jemdoc.py wexp.jemdoc 
+./jemdoc.py links.jemdoc 
+./jemdoc.py Interests.jemdoc 
+./jemdoc.py index.jemdoc 
+./jemdoc.py awards.jemdoc
+./jemdoc.py treks-trips.jemdoc
+./jemdoc.py moreinfo.jemdoc
+./jemdoc.py talks.jemdoc
+./jemdoc.py courseprojects.jemdoc
+
